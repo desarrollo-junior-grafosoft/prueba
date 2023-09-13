bd60e8a86fbdf3b6756beed084353cd592cd1d8b
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-09-13 16:16:51 -0500

    Inicio del repositorio para la pueba

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './header.css';
+
+export const Header = ()=>{
+    return <div className="padre">
+        <div>
+
+        </div>
+        <div>
+            <div className="nuevoLibro">
+                <h2>Hola, Andres, ¿Estas inspirado hoy?</h2>
+                <p>Vuelte tu libro una tendenca mundial</p>
+                <p>Quieres empezar a escribir una vez mas?</p>
+                <button > Crea tu Borrador <span> + </span></button>
+            </div>
+            <div>
+                <img src="" alt="" />
+            </div>
+        </div>
+    </div>
+}
